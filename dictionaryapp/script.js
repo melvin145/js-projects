b7a46da5e1872457ff34067546ce8f0004b7ceeb
melvin145 <pythonnoobie4@gmail.com>
2023-05-01 11:57:05 +0530

    changed js

--- dictionaryapp/script.js
+++ dictionaryapp/script.js
@@ -6,15 +6,13 @@ const word=document.getElementById("word");
 const example=document.getElementById("example");
 
 
-console.log("melvin");
 btn.addEventListener("click",()=>{
       const inpword=document.getElementById("inp-word").value;
       const url= 'https://api.dictionaryapi.dev/api/v2/entries/en/'+inpword;
       fetch(url)
       .then(response =>response.json())
       .then(data=>
-            {console.log(data[0].meanings[0]);
-             console.log(data[0]);
+            {
              meaning.innerHTML=data[0].meanings[0].definitions[0].definition;
              meaning2.innerHTML=data[0].meanings[0].definitions[1].definition;
              word.innerHTML=data[0].word;
