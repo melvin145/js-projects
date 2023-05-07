7c5f875c46b1bd3d0984ae20c6b3129269f7d802
melvin145 <pythonnoobie4@gmail.com>
2023-05-07 21:38:18 +0530

    second commit

--- Memory Game/script.js
+++ Memory Game/script.js
@@ -1,2 +1,11 @@
-const items=["🍊"," 🍌","🐶","🚑","🐷","🍔"]
-console.log(items);
+const items=["🍊"," 🍌","🐶","🚑","🐷","🍔","🍊"," 🍌","🐶","🚑","🐷","🍔"]
+const board=document.querySelector(".board__container");
+items.sort(()=>0.5-Math.random());
+console.log(board);
+
+function Board(){
+      for(let i=0;i<items.length;i++){
+      board.insertAdjacentHTML("beforeend",`<div class="items"><h1>${items[i]}</h1></div>`);
+}
+}
+Board();
