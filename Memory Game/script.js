d5b5a6568d7f96d9aa031a81752955d10a0a2908
melvin145 <pythonnoobie4@gmail.com>
2023-05-10 20:21:27 +0530

    V

--- Memory Game/script.js
+++ Memory Game/script.js
@@ -1,27 +1,80 @@
-const items=["🍊"," 🍌","🐶","🚑","🐷","🍔","🍊"," 🍌","🐶","🚑","🐷","🍔"]
+//const items=["🍊"," 🍌","🐶","🚑","🐷","🍔","🍊"," 🍌","🐶","🚑","🐷","🍔"]
+
+const items=[{
+         'name':'orange',
+         'value': '🍊',     
+},{
+      'name':'banana',
+      'value':'🍌'
+}
+,
+{
+      'name':'dog',
+      'value':'🐶'
+},{
+      'name':'ambulance',
+      'value':'🚑'
+},{
+      'name':'dog',
+      'value':'🐷'
+}
+,{
+  'name':'burger',
+  'value':'🍔'      
+},{
+'name':'orange2',
+'value': '🍊',     
+},{
+'name':'banana2',
+'value':'🍌'
+}
+,
+{
+'name':'dog2',
+'value':'🐶'
+},{
+'name':'ambulance2',
+'value':'🚑'
+},{
+'name':'dog2',
+'value':'🐷'
+}
+,{
+'name':'burger2',
+'value':'🍔'      
+}
+]
+console.log(items[0]['name'])
 const board=document.querySelector(".board__container");
+const move=document.getElementById("move");
 items.sort(()=>0.5-Math.random());
-console.log(board);
 
 function Board(){
       for(let i=0;i<items.length;i++){
       board.insertAdjacentHTML("beforeend",`<div class="items">
-      <h1>${items[i]}</h1></div>`);
+      <h1 onclick="gameCheck(${items[i]})">${items[i]['value']}</h1></div>`);
+
       }
       gameChecker();
 }
 
+
+function gameCheck(){
+      
+}
 function gameChecker(){
      let SelectedItem=[];
+     let moveCount=0;
      const item=document.querySelectorAll(".items")
      item.forEach((e)=>e.addEventListener("click",()=>{
             e.classList.add("active")
             SelectedItem.push(e);
+            moveCount+=1;
+            move.innerText=moveCount;
             if(SelectedItem.length==2){
                   Score(SelectedItem[0],SelectedItem[1]);
                   SelectedItem=[]
             }
-            console.log(SelectedItem)
      }))
 
 }
@@ -29,17 +82,13 @@ Board();
 
 function Score(value1,value2){
       if(value1.innerText==value2.innerText){
-            console.log("helloo",value1);
-            value1.classList.add("item-remove")
-            value2.classList.add("item-remove");
+            value1.classList.add('item-remove');
+            value2.classList.add('item-remove');
       }
       else{
             setTimeout(()=>{
-                  value1.classList.remove("active");
-                  value2.classList.remove("active");
-                  console.log("not equal");
-            },500);
-            
+                  value1.classList.remove('active');
+                  value2.classList.remove('active');
+            },1200);
       }
-
 }
