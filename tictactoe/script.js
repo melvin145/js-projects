90a8e3c07cdbefed2c8efda8caed0688a1b9993c
melvin145 <pythonnoobie4@gmail.com>
2023-05-28 19:23:10 +0530

    final commit

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -1,4 +1,6 @@
 const column=document.querySelectorAll('.column')
+let statusScreen=document.getElementById("status");
+let restartbtn=document.getElementById("restart")
 let winConditions=[
       [0,1,2],
       [3,4,5],
@@ -16,6 +18,7 @@ CheckClick()
 function CheckClick(){
       column.forEach(col=>col.addEventListener("click",
       UpdateBoard,{once:true}))
+      restartbtn.addEventListener("click",RestartGame);
 }
 
 function UpdateBoard(){
@@ -25,22 +28,41 @@ function UpdateBoard(){
             options[cellIndex]=player;
       }
       player=player=="X"?"O":"X";
+      statusScreen.innerText=`${player}'s turn`;
       let winner=CheckWin();
       console.log(winner);
 }
 function CheckWin(){
+      let roundwon=false;
       for(let i=0;i<winConditions.length;i++){
             condition=winConditions[i];
-            cellA=condition[0];
-            cellB=condition[1];
-            cellC=condition[2];
-            console.log(options[cellA],options[cellB],options[cellC])
-            if(options[cellA]==" " && options[cellB]==" " && options[cellC]==" "){
+            cellA=options[condition[0]];
+            cellB=options[condition[1]];
+            cellC=options[condition[2]];
+            console.log(cellA,cellB,cellC)
+            if(cellA==" " || cellB ==" " || cellC==" "){
                   continue;
 
             }
-            else if(options[cellA]==options[cellB]==options[cellC]){
-                  return player;
+            else if(cellA==cellB && cellB==cellC){
+                  roundwon=true
+                  break;
             }
       }
+      if(roundwon){
+            statusScreen.innerText=`${cellA} won`;
+            roundwon=false;
+      }
+      else if(!player.includes("")){
+            statusScreen.innerText="Draw"
+      }
+}
+
+function RestartGame(){
+      player="X";
+      options=[" "," "," "," "," "," "," "," "," "];
+      statusScreen.innerText="X's turn";
+      column.forEach((col)=>{
+            col.innerText="";
+      })
 }
