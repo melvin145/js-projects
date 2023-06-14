8b43156c295f9233ae5604d09eb30ee7cae74e61
melvin145 <pythonnoobie4@gmail.com>
2023-06-14 21:05:30 +0530

    m

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -3,13 +3,17 @@ const api_url='https://api.weatherapi.com/v1/current.json?key=ded3085a2bb042a6a3
 async function getapi(api_url){
       let response=await fetch(api_url);
       result = await response.json();
-      return result;
+      let api_data={
+            "temp":result['current']['temp_c'],
+            "city":result['location']['name'],
+            "climate":result['current']['condition']['text'],
+      }
+      return await response.json();
 }
 
 function ShowDetails(){
       const temp=document.getElementById("tempreature");
       const city=document.getElementById("city");
-      result=getapi(api_url);
-      console.log(result)
+      let data=
 }
 ShowDetails()
