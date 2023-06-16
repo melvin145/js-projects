034e0141b10918c2518af84de27c0f628ad50654
melvin145 <pythonnoobie4@gmail.com>
2023-06-16 19:14:44 +0530

    e

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -1,11 +1,11 @@
 
-const api_url='https://api.weatherapi.com/v1/current.json?key=ded3085a2bb042a6a38165015231306&q=london';
-let response=fetch(api_url).then((response) =>{return response.json()})
+const api_url='https://api.weatherapi.com/v1/current.json?key=ded3085a2bb042a6a38165015231306&q=japan';
+let response=fetch(api_url).then((response) =>{return response.json()});
 const ShowDetails=async () =>{
       const temp=document.getElementById("tempreature");
       const city=document.getElementById("city");
+      const image=document.getElementById("image");
       const a=await response;
-      console.log(a)
       let data={
             "city":a['location']['name'],
             "temp":a['current']['temp_c'],
