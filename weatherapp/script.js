db83b47fc5b0fbc7cfc2172902e35d4b78e6a54c
melvin145 <pythonnoobie4@gmail.com>
2023-06-16 20:10:42 +0530

    t

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -6,11 +6,20 @@ const ShowDetails=async () =>{
       const city=document.getElementById("city");
       const image=document.getElementById("image");
       const a=await response;
+      console.log(a);   
       let data={
             "city":a['location']['name'],
             "temp":a['current']['temp_c'],
             "climate":a['current']['condition']['text']
       }
+      let weather_img={
+            "sunny":"img/1(1).png",
+            "cloudy":"img/2.png",
+            "Partly cloudy":"img/5.png",
+      }
+      console.log(data['climate'])
+      image.src=weather_img[data['climate']];
+      console.log(image)
       temp.innerText=data["temp"];
       city.innerText=data['city'];
 }
