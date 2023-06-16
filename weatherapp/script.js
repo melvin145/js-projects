66d372cdbe4740ba3531f740db9a5b137170c91d
melvin145 <pythonnoobie4@gmail.com>
2023-06-16 22:15:06 +0530

    rr

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -1,17 +1,14 @@
 const temp=document.getElementById("tempreature");
 const city=document.getElementById("city");
 const image=document.getElementById("image");
-const api_url='https://api.openweathermap.org/data/2.5/weather?q={london}&appid={548e9d7e8915c24ab66b4980740b2bc5}';
-let response=fetch(api_url).then((response) =>{return response.json()});
+const api_key='548e9d7e8915c24ab66b4980740b2bc5';
+const input="London";
+const api_url=`https://api.openweathermap.org/data/2.5/weather?q=London&appid=548e9d7e8915c24ab66b4980740b2bc5&units=metric`;
 
-const ShowDetails=async () =>{
-      const a=await response;
-      console.log(a)
-      let weather_img={
-            "Sunny":"img/1(1).png",
-            "cloudy":"img/2.png",
-            "Partly cloudy":"img/5.png",
+ function getWeather(){
+            fetch(api_url).then((response)=>response.json()).then((data)=>{
+                  console.log(data);
+            })
+            
       }
-}
-
-ShowDetails()
+getWeather()
