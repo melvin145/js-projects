978fb790ef927d10a746e26bcc9698c376d6af18
melvin145 <pythonnoobie4@gmail.com>
2023-06-17 19:26:02 +0530

    rrrr

--- weatherapp/script.js
+++ weatherapp/script.js
@@ -2,8 +2,6 @@
 const searchbtn=document.getElementById("searchbtn");
 const api_key='548e9d7e8915c24ab66b4980740b2bc5';
 const input=document.getElementById("input");
-const api_url=`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${api_key}&units=metric`;
-
 searchbtn.addEventListener('click',getWeather);
 
 
@@ -11,15 +9,24 @@ searchbtn.addEventListener('click',getWeather);
       const temp=document.getElementById("tempreature");
       const city=document.getElementById("city");
       const image=document.getElementById("image");
-      if(input.val==0){
+      const weather=document.getElementById("weather");
+      let weatherdesc=document.getElementById("weatherdesc")
+      let weather_img={
+            "Clouds":"/"
+      }
+      const api_url=`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${api_key}&units=metric`;
+      let inputVal=input.value;
+      if(inputVal.length==0){
             alert("enter the city ");
       }
       else{
+            inputVal="";
             fetch(api_url).then((response)=>response.json()).then((data)=>{
                   console.log(data);
                   temp.innerText=Math.floor(data['main']['temp']);
-                  city.innerText=data['name']
-                  
+                  city.innerText=data['name'];
+                  weather.innerText=data['weather'][0]['main'];
+
             })
       }
             
